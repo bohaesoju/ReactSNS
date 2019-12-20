bc36a0b80e93bcf18540a071f7a392500955c316
bohaesoju <bohaesoju@gmail.com>
2019-12-20 16:52:15 +0900

    first commit

---
+++ front/.next/static/webpack/static/development/pages/index.js.55916d8808c2b22e1d26.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/kimeuijoong/Documents/sideProjects/react-nodebird/front/pages/index.js";
+
+
+var Home = function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Hello, NextJS");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.55916d8808c2b22e1d26.hot-update.js.map
---
+++ front/components/AppLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Menu } from 'antd';
+
+const AppLayout = () => {
+    return(
+        <div>
+            <Menu>
+                <Menu.Item>노드버드</Menu.Item>
+                <Menu.Item>프로필</Menu.Item>
+                <Menu.Item>노드버드</Menu.Item>
+            </Menu>
+        </div>
+    )
+};
+
+export default AppLayout;
