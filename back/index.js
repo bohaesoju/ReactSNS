a7d6233ad9f36e884d80e7b147b40aaddf2da9d0
kimuyjoong <fr_bohaesoju@woowahan.com>
2020-04-10 09:45:46 +0900

    backend router

--- back/index.js
+++ back/index.js
@@ -40,6 +40,10 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res) => {
+    res.send('react nodebird 백엔드 정상 동작!')
+});
+
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
 app.use('/api/posts', postsAPIRouter);
